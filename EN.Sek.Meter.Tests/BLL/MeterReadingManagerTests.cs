5023af050addc95ba0082ffeabc99645cc39ddf8
Steve <steve.haran@mac.com>
2024-11-03 11:19:30 +0000

    Update MeterReadingManagerTests.cs

--- EN.Sek.Meter.Tests/BLL/MeterReadingManagerTests.cs
+++ EN.Sek.Meter.Tests/BLL/MeterReadingManagerTests.cs
@@ -43,6 +43,28 @@ public class MeterReadingManagerTests
 		}
 	}
 
+	[DataTestMethod]
+	[DataRow("AccountId,MeterReadValue,MeterReadingDateTime\n1,10000,2023-01-01T00:00:00\n2,20000,2023-01-01T00:00:00\n3,30000,2023-01-01T00:00:00", true, "")]
+	public async Task BulkMeterReadingAsync_BulkLoad_ReturnsExpectedResponse(string csvRow, bool isValid, string failReason)
+	{
+		// Arrange
+		var csvContent = csvRow;
+		var csvBytes = System.Text.Encoding.UTF8.GetBytes(csvContent);
+		var formFile = new FormFile(new MemoryStream(csvBytes), 0, csvBytes.Length, "meterReadingCSV", "meterReading.csv");
+
+		var _mockMeterReadingValidator = new Mock<IMeterReadingValidator>();
+		_mockMeterReadingValidator.Setup(x => x.ValidateMeterReadingAsync(It.IsAny<BulkMeterReadingCSV>())).ReturnsAsync((isValid, failReason));
+
+		var _meterReadingManager = CreateManager(moqMeterReadingValidator: _mockMeterReadingValidator);
+
+		// Act
+		var response = await _meterReadingManager.BulkMeterReadingAsync(formFile);
+
+		// Assert
+		Assert.IsNotNull(response);
+		Assert.AreEqual(3, response.SuccessCount);
+	}
+
 	private MeterReadingManager CreateManager(
 		IMock<IMeterReadingDataProvider> moqMeterReadingDataProvider = null,
 		IMock<IMeterReadingValidator> moqMeterReadingValidator = null)
